6ba1f444e98ce445b3bf29ec323861d82d6e1e9a
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-10-25 15:05:02 +0300

    footer and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        Navbar
+        <Navbar />
         <main>{children}</main>
-        Footer
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import AuthProviders from "./AuthProviders";
+
+export const Navbar = () => {
+  const session = {};
+
+  return (
+    <nav className="fleBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            width={115}
+            height={43}
+            alt="Flexible"
+            loading="lazy"
+          />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
+};
