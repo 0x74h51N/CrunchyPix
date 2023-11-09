f18518f535466c0ad932401acf4a8245eac942c5
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-11-09 15:56:35 +0300

    style fix

--- components/SlideBox.tsx
+++ components/SlideBox.tsx
@@ -16,8 +16,10 @@ const SlideBox = ({
     active && (
       <div
         className={`${
-          left ? "animate-slideLeft right-0" : "animate-slideRight left-0"
-        } opacity-0 absolute bottom-24  w-auto h-auto flex flex-col justify-center items-center bg-stone-900 bg-opacity-30 rounded-bl-xl rounded-tl-xl hover:bg-opacity-60 hover:transition-all duration-1000 ease-in-out z-30 px-24 py-14 backdrop-blur-sm`}
+          left
+            ? "animate-slideLeft right-0 rounded-bl-xl rounded-tl-xl"
+            : "animate-slideRight left-0 rounded-br-xl rounded-tr-xl"
+        } opacity-0 absolute bottom-24  w-auto h-auto flex flex-col justify-center items-center bg-stone-900 bg-opacity-30  hover:bg-opacity-60 hover:transition-all duration-1000 ease-in-out z-30 px-24 py-14 backdrop-blur-sm`}
       >
         <p className="text-start text-stone-200 text-xl font-medium">{title}</p>
         <p className="text-start text-stone-200 text-l font-normal mt-2 max-w-sm">
