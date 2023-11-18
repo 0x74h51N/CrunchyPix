f2b27b2d52820842d95419d836b7d73b84fce6b9
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-11-18 11:05:53 +0300

    langfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html>
       <body>
         <Suspense fallback={<LoadingComponent />}>
           <AppReduxProvider>
