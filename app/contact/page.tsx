f3178d7748c6c3e45cae406b76d2d73e757aea19
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-12-01 02:19:43 +0300

    pre-footer

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -28,52 +28,23 @@ const page = () => {
     return null;
   }
   return (
-    <section>
-      <motion.div
-        variants={staggerContainer(2, 2)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true, amount: 0.2 }}
-        className="relative overflow-hidden"
-      >
-        <div className="lg:px-10 pt-14 pe-6 px-5  ">
-          <div className="flexBetween max-md:flex-col w-full z-10">
-            <motion.div
-              variants={slideIn("left", "tween", 0.3, 1)}
-              className="flex flex-col gap12 z-10 xl:w-1/5 w-1/4 max-md:w-full px-8"
-            >
-              <Image
-                src="/LogoL.svg"
-                width={250}
-                height={100}
-                loading={"lazy"}
-                alt="Flexibble"
-                className="min-w-[180px] h-auto"
-              />
-              <div className="flex items-start flex-col w-full">
-                <p className="text-start text-stone-200 text-l font-medium mt-5 whitespace-normal">
-                  {t("footer.title")}
-                </p>
-                <p className="text-start text-stone-200 text-sm font-normal mt-3 max-md:w-3/5">
-                  {t("footer.description")}
-                </p>
-              </div>
-            </motion.div>
-            <div className="flex flex-col p-8 justify-center gap-7 z-10 w-3/6 max-lg:mx-6 max-xl:w-4/6 max-md:w-full max-xs:px-8">
-              <motion.div
-                variants={slideIn("right", "tween", 0.3, 1)}
-                className="flex-[0.75]"
-              >
-                <Contact />
-              </motion.div>
-            </div>
-          </div>
-          <div className="text-neutral-200 flexBetween mt-10 footer_copyright">
-            <p>@ 2023 Tahsin O.. All right reserved</p>
-          </div>
-        </div>
-      </motion.div>
-    </section>
+    <motion.div
+      variants={staggerContainer(2, 2)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.2 }}
+      className="w-full flex flex-row  overflow-hidden min-h-[100svh]"
+    >
+      <div className="w-4/6"></div>
+      <div className="flex flex-col p-8 justify-end items-center gap-7 z-10 w-2/6 max-xs:px-8">
+        <motion.div
+          variants={slideIn("right", "tween", 0.3, 1)}
+          className="w-full"
+        >
+          <Contact />
+        </motion.div>
+      </div>
+    </motion.div>
   );
 };
 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Image from "next/image";
-import { Links } from "@/constants";
-import Link from "next/link";
-import Contact from "./Contact";
+
 import { lazy, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion";
@@ -10,6 +8,9 @@ import { slideIn, staggerContainer } from "@/utils/motion";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { setIsTranslationsLoaded } from "@/store/redux/language";
+import { ColorfulBorder } from "./ColorfulBorder";
+import { SocialIcons } from "./SocialIcons";
+import { socialIcons } from "@/constants/socialIcons";
 
 const Footer = () => {
   const { t, i18n } = useTranslation(["translation"]);
@@ -30,7 +31,7 @@ const Footer = () => {
     return null;
   }
   return (
-    <div className="flex justify-center ">
+    <div className="flex justify-center footer">
       <motion.div
         variants={staggerContainer(2, 2)}
         initial="hidden"
@@ -38,55 +39,47 @@ const Footer = () => {
         viewport={{ once: true, amount: 0.2 }}
         className="relative overflow-hidden"
       >
-        <div className="lg:px-10 pt-14 pe-6 px-5  ">
-          <div className="flexBetween max-md:flex-col w-full z-10">
+        <div className="lg:px-10 pt-14 pe-6 px-5">
+          <div className="flex flexBetween max-md:flex-col w-full h-auto z-10">
             <motion.div
               variants={slideIn("left", "tween", 0.3, 1)}
-              className="flex flex-col gap12 z-10 xl:w-1/5 w-1/4 max-md:w-full px-8"
+              className="flex flex-row max-lg:flex-col gap-12 z-10 w-auto px-10"
             >
               <Image
                 src="/LogoL.svg"
                 width={250}
                 height={100}
-                loading={"lazy"}
+                loading={"eager"}
                 alt="Flexibble"
                 className="min-w-[180px] h-auto"
               />
-              <div className="flex items-start flex-col w-full">
-                <p className="text-start text-stone-200 text-l font-medium mt-5 whitespace-normal">
-                  {t("footer.title")}
-                </p>
-                <p className="text-start text-stone-200 text-sm font-normal mt-3 max-md:w-3/5">
-                  {t("footer.description")}
-                </p>
-              </div>
-              <div className="flex flex-wrap gap-10 mt-5 z-10">
-                <div className="text-neutral-200 flex-1 flex flex-col gap-4">
-                  <div>
-                    <ul>
-                      {Links.map((link) => (
-                        <li key={link.key}>
-                          <Link
-                            href={link.href}
-                            key={link.key}
-                            className={`w-20 block text-sm  text-neutral-200 antialiased mt-2 hover:text-log-col hover:border-b hover:border-log-col`}
-                          >
-                            {t(link.text)}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
+              <div className="flex flex-row gap-8 items-center">
+                <SocialIcons iconPack={socialIcons} row={true} />
               </div>
             </motion.div>
-            <div className="flex flex-col p-8 justify-center gap-7 z-10 w-3/6 max-lg:mx-6 max-xl:w-4/6 max-md:w-full max-xs:px-8">
+            <div className="flex flex-row max-lg:flex-col items-end justify-end max-lg:items-center max-lg:justify-center gap-4 w-auto h-full">
               <motion.div
                 variants={slideIn("right", "tween", 0.3, 1)}
-                className="flex-[0.75]"
+                className="flex flex-col items-end justify-end max-lg:items-center  p-2 w-auto max-w-[30svw] max-lg:max-w-full "
               >
-                <Contact />
+                <p className="text-stone-200 text-l font-medium mt-5 whitespace-normal max-md:text-center text-end">
+                  {t("footer.title")}
+                </p>
+                <p className="text-stone-200 text-sm font-normal mt-3 w-full max-md:text-center text-end">
+                  {t("footer.description")}
+                </p>
               </motion.div>
+
+              <div className="h-full w-auto max-w-[110px] z-0">
+                <ColorfulBorder>
+                  <a
+                    href="/contact"
+                    className="w-full rounded-2xl bg-transparent p-4 text-cool-gray-100 hover:text-log-col text-center"
+                  >
+                    Contact with me!
+                  </a>
+                </ColorfulBorder>
+              </div>
             </div>
           </div>
           <div className="text-neutral-200 flexBetween mt-10 footer_copyright">
