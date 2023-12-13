b5d33b878f4d614ece54563ce373b185fc1790d5
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-12-13 23:12:28 +0300

    cookiePolicy

---
+++ components/PolicyCreator/PolicyCreator.tsx
@@ -0,0 +1,61 @@
+import { Policy } from "@/app/common.types";
+import { RootState } from "@/store";
+import { setIsTranslationsLoaded } from "@/store/redux/language";
+import React, { memo, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useSelector, useDispatch } from "react-redux";
+
+const PolicyCreator = memo(({ data }: { data: Policy[] }) => {
+  const { t, i18n } = useTranslation(["translation"]);
+  const isTranslationsLoadedRedux = useSelector(
+    (state: RootState) => state.language.isTranslationsLoaded
+  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (i18n.isInitialized) {
+      dispatch(setIsTranslationsLoaded(true));
+    } else {
+      i18n.on("initialized", () => {
+        dispatch(setIsTranslationsLoaded(true));
+      });
+    }
+  }, [i18n, dispatch]);
+  if (!isTranslationsLoadedRedux) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-10">
+      {data.map((item, index) => (
+        <div key={index}>
+          {item.mainTitle && (
+            <>
+              <h1 className="h1 text-center">{t(item.mainTitle)}</h1>
+              <p className="p mt-2">{t(item.description)}</p>
+            </>
+          )}
+          {item.title && (
+            <>
+              <h2 className="h2">{t(item.title)}</h2>
+              <p className="p mt-2">{t(item.description)}</p>
+            </>
+          )}
+          {item.subTitles && (
+            <ul>
+              {item.subTitles.map((subItem, subIndex) => (
+                <li className="ml-6 mt-3 list-disc text-white" key={subIndex}>
+                  <h3 className="h3 underline underline-offset-2">
+                    {t(subItem.title)}
+                  </h3>
+                  <p className="p ml-2">{t(subItem.description)}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+});
+
+export default PolicyCreator;
---
+++ constants/cookiePolicyData.ts
@@ -0,0 +1,38 @@
+import { Policy } from "@/app/common.types";
+
+export const cookiePolicyData: Policy[] = [
+  {
+    mainTitle: "cookiePolicy.0.mainTitle",
+    description: "cookiePolicy.0.description",
+  },
+  {
+    title: "cookiePolicy.1.title",
+    description: "cookiePolicy.1.description",
+  },
+  {
+    title: "cookiePolicy.2.title",
+    description: "cookiePolicy.2.description",
+    subTitles: [
+      {
+        title: "cookiePolicy.2.subTitles.0.title",
+        description: "cookiePolicy.2.subTitles.0.description",
+      },
+      {
+        title: "cookiePolicy.2.subTitles.1.title",
+        description: "cookiePolicy.2.subTitles.1.description",
+      },
+    ],
+  },
+  {
+    title: "cookiePolicy.3.title",
+    description: "cookiePolicy.3.description",
+  },
+  {
+    title: "cookiePolicy.4.title",
+    description: "cookiePolicy.4.description",
+  },
+  {
+    title: "cookiePolicy.5.title",
+    description: "cookiePolicy.5.description",
+  },
+];
