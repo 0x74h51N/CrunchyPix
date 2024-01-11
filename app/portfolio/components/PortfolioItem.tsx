508d207abffb6d39196415ff34b0da8700e30b7f
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2024-01-11 14:35:53 +0300

    touchFix

--- app/portfolio/components/PortfolioItem.tsx
+++ app/portfolio/components/PortfolioItem.tsx
@@ -12,13 +12,6 @@ import { FaAnglesRight } from "react-icons/fa6";
 import { clickableChange } from "@/store/redux/isClickable";
 import { PortfolioItemProps } from "@/app/common.types";
 
-function isTouchScreen() {
-  if (typeof window !== "undefined") {
-    return "ontouchstart" in window || navigator.maxTouchPoints > 0;
-  }
-  return false;
-}
-
 const PortfolioItem = ({
   _id,
   image,
@@ -27,7 +20,7 @@ const PortfolioItem = ({
   projectType,
 }: PortfolioItemProps) => {
   const isMobile = useSelector((state: RootState) => state.isMobile.mobile);
-  const isTouch = isTouchScreen();
+  const isTouch = useSelector((state: RootState) => state.isTouch.touch);
   const originalWidth = 630;
   const originalHeight = 500;
   const mobileWidth = 320;
@@ -97,7 +90,7 @@ const PortfolioItem = ({
         />
 
         <div className="absolute w-full h-full  group-hover:backdrop-filter group-hover:backdrop-blur-sm bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-500 ease-in-out rounded-xl " />
-        {isTouch ? (
+        {isTouch /**eğer dokunmatik ekran varsa sadece Link öğesi oluşsun. Eğer dokunmatik değilse, motion.div ve Link öğesi oluşsun istiyorum. */ ? (
           <Link
             href={`/portfolio/${id}`}
             passHref
---
+++ store/redux/isTouch.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+interface isTouch {
+  touch: boolean;
+}
+
+const initialState: isTouch = {
+  touch: false,
+};
+const touchSlice = createSlice({
+  name: "isTouch",
+  initialState,
+  reducers: {
+    setTouch: (state, action) => {
+      state.touch = action.payload;
+    },
+  },
+});
+
+export const { setTouch } = touchSlice.actions;
+export default touchSlice.reducer;
