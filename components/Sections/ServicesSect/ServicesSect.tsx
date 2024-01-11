6930d62720a44638107600aab543bbc6b37dd0ee
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2024-01-11 16:02:05 +0300

    serviceCard fix

--- components/Sections/ServicesSect/ServicesSect.tsx
+++ components/Sections/ServicesSect/ServicesSect.tsx
@@ -57,10 +57,10 @@ const ServicesSect = () => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.6 }}
-        className="flex flex-col items-start h-full w-auto md:p-16 xs:p-6 p-2 z-10 mt-14 rounded-lg 2xl:ml-36 bg-neutral-950 shadow-lg shadow-black"
+        className="flex flex-col items-start h-full w-auto md:p-16 xs:p-6 p-4 z-10 mt-14 rounded-lg 2xl:ml-36 bg-neutral-950 shadow-lg shadow-black "
       >
         <motion.h1 variants={slideIn("left", "spring", 0.5, 1)}>
-          <div className="h2">
+          <div className="h2 max-sm:ml-4">
             {isMobile || isTablet
               ? t("servicesSect.intro")
               : generateSpans({
@@ -69,7 +69,7 @@ const ServicesSect = () => {
                   zeroColor: "#909090",
                 })}
           </div>
-          <div className="h1 mb-2">
+          <div className="h1 mb-2 max-sm:ml-4">
             {isMobile || isTablet
               ? t("servicesSect.title")
               : generateSpans({
@@ -113,7 +113,7 @@ const ServicesSect = () => {
         </motion.div>
       </motion.div>
       <div
-        className="custom-pagy absolute cursor-none left-0 bottom-0 z-30 flex 2xl:flex-col flex-row justify-center items-center h-auto 2xl:min-h-[100svh] shadow-xl shadow-black
+        className="custom-pagy absolute cursor-none left-0 bottom-0 z-30 flex 2xl:flex-col flex-row justify-center items-center h-auto 2xl:min-h-[100svh] shadow-xl 2xl:shadow-black
         w-full 2xl:max-w-[180px] 2xl:bg-neutral-950 2xl:p-40 p-10 2xl:gap-8 gap-4"
       />
     </div>
