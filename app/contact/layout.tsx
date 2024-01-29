4fec527a80a284072ef75cbe990c9e49942e7f5a
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2024-01-29 12:55:10 +0300

    pageImages, portfolio components, nav fix

---
+++ app/contact/layout.tsx
@@ -0,0 +1,22 @@
+import Rooting from "@/components/Rooting";
+
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Rooting />
+      <main>{children}</main>
+    </>
+  );
+}
+
+// export async function generateStaticParams() {
+//   const paths = portfolioPageItems.map((item) => ({
+//     params: { id: item._id.toLowerCase().replace(/\s+/g, "") },
+//   }));
+
+//   return paths;
+// }
---
+++ app/portfolio/[id]/components/TopImage.tsx
@@ -0,0 +1,45 @@
+import { Icon } from "@/app/common.types";
+import IconButton from "@/components/Buttons/IconButton";
+import { slideIn } from "@/utils/motion";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const TopImage = ({
+  imageTop,
+  imageAlt,
+  icons,
+}: {
+  imageTop: string;
+  imageAlt: string;
+  icons: Icon[];
+}) => {
+  return (
+    <div className="relative w-full md:h-auto md:min-h-[650px] h-[350px]">
+      <Image
+        width={1850}
+        height={1850}
+        quality={100}
+        loading="lazy"
+        src={imageTop}
+        alt={imageAlt}
+        className="w-full h-full object-center md:object-contain object-cover bg-gradient-to-br from-neutral-900  to-slate-700"
+      />
+
+      <motion.div
+        variants={slideIn("right", "spring", 2, 1)}
+        className="absolute flex flex-row gap-4 bottom-5 py-3 right-0 pr-6 pl-4 bg-black bg-opacity-50 rounded-l-lg"
+      >
+        {icons.map((icon, iconIndex) => (
+          <span
+            key={iconIndex}
+            className="hover:text-log-col transition-all ease-in-out duration-300 text-cool-gray-50 lg:text-4xl text-2xl"
+          >
+            <IconButton key={iconIndex} icon={icon} />
+          </span>
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default TopImage;
