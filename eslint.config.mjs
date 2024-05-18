488a49291ad6b7b7fa0806a2c739dd785be34bca
0x74h51N <mr.tahsin@windowslive.com>
2024-05-18 16:58:41 +0300

    eslint extend fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginTypescript from "@typescript-eslint/eslint-plugin";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import importPlugin from "eslint-plugin-import";
-import unusedImports from "eslint-plugin-unused-imports";
 import typescriptParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
+import pluginImport from "eslint-plugin-import";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
 
 const { configs: tsConfigs } = eslintPluginTypescript;
 
 export default [
-  { 
-    languageOptions: { 
+  {
+    languageOptions: {
       globals: globals.browser,
       parser: typescriptParser,
       parserOptions: {
@@ -25,38 +26,38 @@ export default [
   },
   pluginJs.configs.recommended,
   tsConfigs.recommended,
-  pluginReactConfig,
-  jsxA11y.configs.recommended,
-  importPlugin.configs.errors,
-  importPlugin.configs.warnings,
-  importPlugin.configs.typescript,
+  pluginReact.configs.recommended,
+  pluginJsxA11y.configs.recommended,
+  pluginImport.configs.errors,
+  pluginImport.configs.warnings,
+  pluginImport.configs.typescript,
   {
     plugins: {
-      react: "eslint-plugin-react",
-      "react-hooks": "eslint-plugin-react-hooks",
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
       "@typescript-eslint": eslintPluginTypescript,
-      "jsx-a11y": "eslint-plugin-jsx-a11y",
-      "import": "eslint-plugin-import",
-      "unused-imports": unusedImports,
+      "jsx-a11y": pluginJsxA11y,
+      "import": pluginImport,
+      "unused-imports": pluginUnusedImports,
     },
     rules: {
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'import/order': ['error', { 'newlines-between': 'always' }],
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "import/order": ["error", { "newlines-between": "always" }],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
       ],
-      'react/jsx-no-useless-fragment': 'warn',
-      'react/jsx-no-bind': 'warn',
-      'jsx-a11y/anchor-is-valid': 'warn',
+      "react/jsx-no-useless-fragment": "warn",
+      "react/jsx-no-bind": "warn",
+      "jsx-a11y/anchor-is-valid": "warn",
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
-  }
+  },
 ];
