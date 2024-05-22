c6a1b1dbf7ed86038bf6f39b2f2a765a076d8219
0x74h51N <mr.tahsin@windowslive.com>
2024-05-22 17:28:47 +0300

    hotfix

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -15,14 +15,14 @@ export async function generatePageMetadata(page: string): Promise<Metadata> {
       title: t('meta.title'),
       description: t('meta.description'),
       url: pageUrl,
-      images: [
-        {
-          url: '@/public/portfolioSlides/crunchypix.jpg',
-          width: 1200,
-          height: 630,
-          alt: t('meta.title'),
-        },
-      ],
+      // images: [
+      //   {
+      //     url: `${baseUrl}/portfolioSlides/crunchypix.jpg`,
+      //     width: 1200,
+      //     height: 630,
+      //     alt: t('meta.title'),
+      //   },
+      // ],
     },
     authors: [{ name: "Tahsin Önemli", url: "https://github.com/0x74h51N" }]
   };
