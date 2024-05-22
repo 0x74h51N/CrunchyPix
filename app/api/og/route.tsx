2706ee3db8f9c5704f7627d532c9f91612573f03
0x74h51N <mr.tahsin@windowslive.com>
2024-05-22 16:31:44 +0300

    fix

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
 
-export const config = {
-  runtime: 'nodejs',
-};
+export const runtime= 'nodejs';
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
