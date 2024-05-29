addd2d0fb7370d081910f54d49c350dbc6d5ff48
0x74h51N <mr.tahsin@windowslive.com>
2024-05-29 19:07:33 +0300

    vscode settings & blur improvement

--- app/portfolio/[id]/components/TopImage.tsx
+++ app/portfolio/[id]/components/TopImage.tsx
@@ -1,11 +1,12 @@
-import { Icon } from "@/app/common.types";
-import IconButton from "@/components/Buttons/IconButton";
-import useBlurUrl from "@/hooks/useBlurUrl";
-import { RootState } from "@/store";
-import { slideIn } from "@/utils/motion";
-import { motion } from "framer-motion";
-import { CldImage } from "next-cloudinary";
-import { useSelector } from "react-redux";
+import { Icon } from '@/app/common.types'
+import IconButton from '@/components/Buttons/IconButton'
+import useBlurUrl from '@/hooks/useBlurUrl'
+import { RootState } from '@/store'
+import { slideIn } from '@/utils/motion'
+import { motion } from 'framer-motion'
+import { CldImage } from 'next-cloudinary'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const TopImage = ({
   imageTop,
@@ -13,44 +14,54 @@ const TopImage = ({
   imageAlt,
   icons,
 }: {
-  imageTop: string;
-  imageTopMobile?: string;
-  imageAlt: string;
-  icons?: Icon[];
+  imageTop: string
+  imageTopMobile?: string
+  imageAlt: string
+  icons?: Icon[]
 }) => {
-  const screenWidth = useSelector(
-    (state: RootState) => state.screenWidth.width
-  );
+  const screenWidth = useSelector((state: RootState) => state.screenWidth.width)
+  const [loading, setLoading] = useState(true)
+  const blurUrl = useBlurUrl(
+    screenWidth <= 768 && imageTopMobile ? imageTopMobile : imageTop,
+  )
 
-  const blurUrl = useBlurUrl(screenWidth <= 768 && imageTopMobile ? imageTopMobile : imageTop)
-  
   return (
     <div
       className={`relative w-full h-auto overflow-hidden ${
-        imageTop.includes("catalog")
-          ? "lg:min-h-[870px] md:min-h-[700px]"
-          : "md:min-h-[700px]"
+        imageTop.includes('catalog')
+          ? 'lg:min-h-[870px] md:min-h-[700px]'
+          : 'md:min-h-[700px]'
       } min-h-[600px]`}
       style={{
-        backgroundImage: imageTop.includes("kyk")
-          ? "linear-gradient(to bottom right,  #e2e8f0, #d6d3d1)"
-          : "linear-gradient(to bottom right,  #171717, #334155)",
+        backgroundImage: imageTop.includes('kyk')
+          ? 'linear-gradient(to bottom right,  #e2e8f0, #d6d3d1)'
+          : 'linear-gradient(to bottom right,  #171717, #334155)',
       }}
-    >  {blurUrl  && 
+    >
+      <CldImage
+        fill
+        sizes="auto"
+        fetchPriority="auto"
+        quality={100}
+        src={screenWidth <= 768 && imageTopMobile ? imageTopMobile : imageTop}
+        alt={imageAlt}
+        onLoad={() => setLoading(false)}
+        className={`w-full h-full object-cover transition-opacity ease-in-out duration-1500 opacity:${loading ? 0 : 100}`}
+      />
+      {blurUrl && loading && (
         <CldImage
           fill
           sizes="auto"
           fetchPriority="auto"
           quality={100}
-          src={screenWidth <= 768 && imageTopMobile ? imageTopMobile : imageTop}
-          alt={imageAlt}
-          placeholder="blur"
-          blurDataURL={blurUrl}
-          className={`w-full h-full object-cover transition-opacity duration-500`}
-        />}
+          src={blurUrl}
+          alt={`${imageAlt}-blur`}
+          className={`w-full h-full object-cover`}
+        />
+      )}
       {icons && (
         <motion.div
-          variants={slideIn("right", "spring", 2, 1)}
+          variants={slideIn('right', 'spring', 2, 1)}
           className="absolute flex flex-row gap-4 bottom-5 py-3 right-0 pr-6 pl-4 bg-black bg-opacity-50 rounded-l-lg"
         >
           {icons.map((icon, iconIndex) => (
@@ -64,7 +75,7 @@ const TopImage = ({
         </motion.div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default TopImage;
+export default TopImage
--- components/RooteTitles/AllRoutes.tsx
+++ components/RooteTitles/AllRoutes.tsx
@@ -1,84 +1,102 @@
-"use client";
-import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
-import MainRoutes from "./MainRoutes";
-import SubRoutes from "./SubRoutes";
-import { CldImage } from "next-cloudinary";
-import useBlurUrl from "@/hooks/useBlurUrl";
-import { AnimatePresence, motion } from "framer-motion";
+'use client'
+import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import MainRoutes from './MainRoutes'
+import SubRoutes from './SubRoutes'
+import { CldImage } from 'next-cloudinary'
+import useBlurUrl from '@/hooks/useBlurUrl'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const AllRoutes = () => {
-  const [mainPage, setMainPage] = useState("");
-  const [childPage, setChildPage] = useState("");
-
-  const pathname = usePathname();
-
+  const [mainPage, setMainPage] = useState('')
+  const [childPage, setChildPage] = useState('')
+  const pathname = usePathname()
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     const updatePageInfo = () => {
-      const urlParts = pathname.split("/");
-      const currentPage = urlParts[1];
+      const urlParts = pathname.split('/')
+      const currentPage = urlParts[1]
       if (urlParts[2]) {
         const currentChildPage = urlParts[2]
-          .replace("_", " ")
-          .replaceAll("-", "");
-        setChildPage(currentChildPage);
-      } else setChildPage("");
-      setMainPage(currentPage);
-    };
+          .replace('_', ' ')
+          .replaceAll('-', '')
+        setChildPage(currentChildPage)
+      } else setChildPage('')
+      setMainPage(currentPage)
+    }
 
-    updatePageInfo();
-    
+    updatePageInfo()
+
     const handlePopState = () => {
-      updatePageInfo();
-    };
+      updatePageInfo()
+    }
 
-    window.addEventListener("popstate", handlePopState);
+    window.addEventListener('popstate', handlePopState)
 
     return () => {
-      window.removeEventListener("popstate", handlePopState);
-    };
-  }, [pathname, setMainPage, setChildPage]);
+      window.removeEventListener('popstate', handlePopState)
+    }
+  }, [pathname, setMainPage, setChildPage])
 
   const blurUrl = useBlurUrl(`crunchypix/${mainPage}.jpg`)
-  if (pathname === "" || pathname === "home" || pathname === "/" || mainPage==="") {
-    return null;
+  if (
+    pathname === '' ||
+    pathname === 'home' ||
+    pathname === '/' ||
+    mainPage === ''
+  ) {
+    return null
   } else {
     return (
       <div
         className="flex flex-col  justify-center items-center w-full lg:h-[380px] md:h-[330px] h-[270px] md:p-10 p-2 overflow-hidden relative"
-        style={{background: mainPage!=='policies'
-          ? "radial-gradient(circle, rgba(0,0,0,0.7), rgba(0,0,0,1))"
-          : "transparent",
-        boxShadow: mainPage!=='policies' ? "inset 0 0 10px 5px rgba(0, 0, 0, 0.8)" : "",
+        style={{
+          background:
+            mainPage !== 'policies'
+              ? 'radial-gradient(circle, rgba(0,0,0,0.7), rgba(0,0,0,1))'
+              : 'transparent',
+          boxShadow:
+            mainPage !== 'policies'
+              ? 'inset 0 0 10px 5px rgba(0, 0, 0, 0.8)'
+              : '',
         }}
       >
-      <AnimatePresence>
-           <motion.div
-              key={mainPage}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.4 }}
-              className="absolute inset-0 z-[-10]"
-            >
-        { blurUrl && 
-        <CldImage
-            src={`crunchypix/${mainPage}.jpg`}
-            alt={mainPage}
-            fill
-            priority
-            sizes="100svw"
-            quality="auto"
-            placeholder="blur"
-            blurDataURL={blurUrl}
-            className="object-cover -z-10 h-[700px]"
-          />}</motion.div>
-          </AnimatePresence>
-        <MainRoutes  childPage={childPage} mainPage={mainPage}/>
+        <AnimatePresence>
+          <motion.div
+            key={mainPage}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
+            className="absolute inset-0 z-[-10]"
+          >
+            <CldImage
+              src={`crunchypix/${mainPage}.jpg`}
+              alt={mainPage}
+              fill
+              priority
+              sizes="100svw"
+              quality="auto"
+              className="object-cover -z-10 h-[700px]"
+              onLoad={() => setLoading(false)}
+            />
+            {blurUrl && loading && (
+              <CldImage
+                src={blurUrl}
+                alt={mainPage}
+                fill
+                sizes="100svw"
+                quality="auto"
+                className="object-cover -z-10 h-[700px]"
+              />
+            )}
+          </motion.div>
+        </AnimatePresence>
+        <MainRoutes childPage={childPage} mainPage={mainPage} />
         <SubRoutes childPage={childPage} mainPage={mainPage} />
       </div>
-    );
+    )
   }
-};
+}
 
-export default AllRoutes;
+export default AllRoutes
