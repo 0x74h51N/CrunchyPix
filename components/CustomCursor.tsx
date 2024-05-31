0434c0feceb7e606fea33f4bb3152d9051d8b78f
0x74h51N <mr.tahsin@windowslive.com>
2024-05-31 21:08:41 +0300

    translate3d perf update

--- components/CustomCursor.tsx
+++ components/CustomCursor.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { RootState } from '@/store'
 import { setTouch } from '@/store/redux/isTouch'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -19,8 +19,9 @@ const CustomCursor = () => {
   const isClickable = useSelector(
     (state: RootState) => state.isClickable.clickable,
   )
-  const { t, i18n } = useTranslation(['index'])
+  const { t } = useTranslation(['index'])
   const dispatch = useDispatch()
+  const requestRef = useRef<number>()
 
   useEffect(() => {
     if (isBrowser && !isTouchDevice) {
@@ -36,76 +37,84 @@ const CustomCursor = () => {
     }
   }, [isBrowser, isTouchDevice, dispatch])
 
+  const updateMousePosition = useCallback(
+    (clientX: number, clientY: number) => {
+      if (cursorRef.current) {
+        cursorRef.current.style.transform = `translate3d(${clientX}px, ${clientY}px, 0)`
+      }
+      if (circleRef.current) {
+        circleRef.current.style.transform = `translate3d(${clientX}px, ${clientY}px, 0)`
+      }
+    },
+    [],
+  )
+
+  const handleMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (isInitialMove) {
+        setIsCursorVisible(true)
+        setIsInitialMove(false)
+      }
+
+      const { clientX, clientY } = e
+      updateMousePosition(clientX, clientY)
+    },
+    [isInitialMove, updateMousePosition],
+  )
+
   useEffect(() => {
     if (!isTouchDevice) {
-      const updateMousePosition = (e: { clientX: number; clientY: number }) => {
-        if (cursorRef.current) {
-          const { clientX, clientY } = e
-          cursorRef.current.style.left = `${clientX}px`
-          cursorRef.current.style.top = `${clientY}px`
-        }
-        if (circleRef.current) {
-          const { clientX, clientY } = e
-          circleRef.current.style.left = `${clientX}px`
-          circleRef.current.style.top = `${clientY}px`
-        }
+      const onMouseMove = (e: MouseEvent) => {
+        requestRef.current = requestAnimationFrame(() => handleMouseMove(e))
       }
 
-      const handleMouseMove = (e: MouseEvent) => {
-        if (isInitialMove) {
-          setIsCursorVisible(true)
-          setIsInitialMove(false)
-        }
-
-        updateMousePosition(e)
-      }
-      document.addEventListener('mousemove', handleMouseMove)
+      document.addEventListener('mousemove', onMouseMove)
 
       return () => {
-        document.removeEventListener('mousemove', handleMouseMove)
+        if (requestRef.current) {
+          cancelAnimationFrame(requestRef.current)
+        }
+        document.removeEventListener('mousemove', onMouseMove)
       }
     }
-  }, [isInitialMove, isTouchDevice])
+  }, [handleMouseMove, isTouchDevice])
 
   if (isTouchDevice || cursorDisabled) {
     return null
-  } else {
-    return (
-      !isTouchDevice && (
-        <div className="relative">
-          <div
-            ref={circleRef}
-            className={`flex items-center justify-center fixed z-[1000] rounded-full border-2
-            border-cool-gray-100 pointer-events-none cursor-none`}
-            style={{
-              transition:
-                'width 300ms ease-in-out, height 300ms, left 74ms ease-out, top 74ms ease-out, backgroundColor 300ms ease-in-out',
-              width: isSlider ? '70px' : isClickable ? '20px' : '45px',
-              height: isSlider ? '70px' : isClickable ? '20px' : '45px',
-              margin: isSlider ? '-7px' : '-20px',
-              visibility: isCursorVisible ? 'visible' : 'hidden',
-            }}
-          />
-          <div
-            ref={cursorRef}
-            className={`flex items-center justify-center fixed z-[999] rounded-full m-[1px] bg-log-col pointer-events-none cursor-none`}
-            style={{
-              transition:
-                'width 300ms ease-in-out, height 300ms ease-in-out, left 60ms ease-out, top 60ms ease-out',
-              width: isSlider ? '55px' : '5px',
-              height: isSlider ? '55px' : '5px',
-              visibility:
-                isCursorVisible && !isClickable ? 'visible' : 'hidden',
-            }}
-          >
-            <span className="transition-all duration-200 text-cool-gray-50 text-justify font-bold text-sm antialised z-[1000]">
-              {isSlider && t('dragQuinn.drag')}
-            </span>
-          </div>
-        </div>
-      )
-    )
   }
+
+  return (
+    <div className="relative">
+      <div
+        ref={circleRef}
+        className={`flex items-center justify-center fixed z-[1000] rounded-full border-2
+        border-cool-gray-100 pointer-events-none cursor-none`}
+        style={{
+          transition:
+            'width 300ms ease-in-out, height 300ms, transform 105ms ease-out, backgroundColor 300ms ease-in-out',
+          width: isSlider ? '70px' : isClickable ? '20px' : '45px',
+          height: isSlider ? '70px' : isClickable ? '20px' : '45px',
+          margin: isSlider ? '-7px' : '-20px',
+          visibility: isCursorVisible ? 'visible' : 'hidden',
+        }}
+      />
+      <div
+        ref={cursorRef}
+        className={`flex items-center justify-center fixed z-[999] rounded-full m-[1px] bg-log-col pointer-events-none cursor-none`}
+        style={{
+          transition:
+            'width 300ms ease-in-out, height 300ms ease-in-out, transform 60ms ease-out',
+          width: isSlider ? '55px' : '5px',
+          height: isSlider ? '55px' : '5px',
+          visibility: isCursorVisible && !isClickable ? 'visible' : 'hidden',
+        }}
+      >
+        <span className="transition-all duration-200 text-cool-gray-50 text-justify font-bold text-sm antialiased z-[1000]">
+          {isSlider && t('dragQuinn.drag')}
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default CustomCursor
