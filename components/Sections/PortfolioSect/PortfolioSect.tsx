55cafb4ab18960df9e05b0033e824be95b97a800
0x74h51N <mr.tahsin@windowslive.com>
2024-06-14 17:24:26 +0300

    sect title

--- components/Sections/PortfolioSect/PortfolioSect.tsx
+++ components/Sections/PortfolioSect/PortfolioSect.tsx
@@ -1,13 +1,12 @@
 'use client';
 import { motion } from 'framer-motion';
-import { generateSpans } from '@/components/GenerateSpans';
 import { slideIn } from '@/utils/motion';
 import dynamic from 'next/dynamic';
 import LoadingComponent from '@/components/Loading/Loading';
-import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
 import { memo } from 'react';
+import TitleText from '../TitleText';
 
 const CarouselSlider = dynamic(
   () => import('@/components/Slider/Portfolio/CarouselSlider'),
@@ -26,7 +25,6 @@ const SlideModal = dynamic(
 );
 
 const PortfolioSect = () => {
-  const { t } = useTranslation('portfolio');
   const portfolioItems = useSelector(
     (state: RootState) => state.portfolio.items,
   );
@@ -34,19 +32,14 @@ const PortfolioSect = () => {
     portfolioItems && (
       <div className="h-auto flex flex-col items-center justify-center w-full">
         <div className="flex flex-col text-center">
-          <motion.h1
+          <motion.div
             variants={slideIn('up', 'spring', 0.5, 1.4)}
             initial="hidden"
             whileInView="show"
             viewport={{ once: true, amount: 'all' }}
-            className="text-stone-200 font-black md:text-[55x] sm:text-[50px] xs:text-[40px] text-[30px] max-w-2xl leading-[60px] z-auto"
           >
-            {generateSpans({
-              text: `${t('projectSlides.title')}`,
-              colorType: 'vibrantColors',
-              zeroColor: '#FFFFFF',
-            })}
-          </motion.h1>
+            <TitleText sectName="portfolio_sect" />
+          </motion.div>
         </div>
         <div className="w-full h-auto min-w-[100sv] z-0 bg-cool-gray-800 max-w-[2200px]">
           <CarouselSlider slides={portfolioItems} />
