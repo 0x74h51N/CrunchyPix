783df8a34e91aaf522c04a5cb9e7c3107e57f6e8
0x74h51N <mr.tahsin@windowslive.com>
2024-08-24 00:11:14 +0300

    seo settings

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/private/', '/public', '/actions/', '/404', '/policies/*'],
+    },
+    sitemap: 'https://crunchypix.com/sitemap.xml',
+  };
+}
