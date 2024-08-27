7801817e79f56ca18d9b83660746d53b3044ae4b
0x74h51N <mr.tahsin@windowslive.com>
2024-08-27 12:03:53 +0300

    slide fix

--- i18n/settings.ts
+++ i18n/settings.ts
@@ -1,13 +1,3 @@
-/**
- * This i18n structure is implemented based on theese articles:
- * https://carlogino.com/blog/nextjs-app-dir-i18n-cookie
- * https://locize.com/blog/next-app-dir-i18n/#step-2
- *
- * Many thanks to the author for the detailed explanation.
- * After implementing the i18n setups and server translation utility,
- * it helped to multi-language metadata w/o sub-language prefixes or different domains.
- */
-
 import type { InitOptions } from 'i18next';
 
 export const FALLBACK_LOCALE = 'en';
