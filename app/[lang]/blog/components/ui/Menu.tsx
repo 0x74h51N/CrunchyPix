fbd77009dedc08f285013f48993c94cbfbc85825
0x74h51N <mr.tahsin@windowslive.com>
2024-09-18 18:16:59 +0300

    hotfix

--- app/[lang]/blog/components/ui/Menu.tsx
+++ app/[lang]/blog/components/ui/Menu.tsx
@@ -36,25 +36,27 @@ const Menu = () => {
       <div
         ref={stickyRef}
         className={clsx(
-          'sticky flex items-center justify-end border border-base-300 backdrop-blur-sm backdrop-filter bg-base-100 top-0 rigth-0 p-2 !select-none z-50 self-end transition-all ease-in-out duration-500',
+          'sticky flex items-center justify-end border border-base-300 backdrop-blur-sm backdrop-filter bg-base-100 top-0 rigth-0 md:p-2 !select-none z-50 self-end transition-all ease-in-out duration-500',
           open
-            ? 'w-full md:pl-6 h-[58px] pr-2 bg-opacity-25'
-            : '-mr-8 w-16 h-22 bg-opacity-100',
+            ? 'w-full md:pl-6 h-[58px] bg-opacity-25'
+            : 'md:-mr-8 sm:-mr-5 -mr-3 md:w-16 w-12 h-22 bg-opacity-100',
         )}
       >
-        <div
-          className={clsx(
-            'justify-between w-full transition-all ease-in-out duration-500 delay-200 flex',
-            open ? 'opacity-100 ' : 'opacity-0 hidden',
-          )}
-        >
-          <div className="flex items-center">
-            <ThemeToggle />
-            <ExpandButton isWide={isWide} setWide={setWide} />
-            <FontSizeChanger />
+        {open && (
+          <div
+            className={clsx(
+              'justify-between w-full transition-all ease-in-out delay-200 duration-200 flex',
+              open ? 'opacity-100 ' : 'opacity-0 hidden',
+            )}
+          >
+            <div className="flex items-center">
+              <ThemeToggle />
+              <ExpandButton isWide={isWide} setWide={setWide} />
+              <FontSizeChanger />
+            </div>
+            <ShareButtons />
           </div>
-          <ShareButtons />
-        </div>
+        )}
 
         <div
           className={clsx(
@@ -63,12 +65,12 @@ const Menu = () => {
         >
           <div
             className={clsx(
-              'py-0 px-auto transition-all w-full max-w-11 ease-in-out duration-200 btn btn-ghost btn-sm h-10',
+              'py-0 px-auto transition-all w-full sm:max-w-11 max-w-8 ease-in-out duration-200 btn btn-ghost btn-sm h-10',
               stickyTop ? 'opacity-0 hidden delay-75' : 'opacity-100',
             )}
             onClick={() => !stickyTop && setOpen(!open)}
           >
-            <div className="ml-2 mt-1.5">
+            <div className="lg:ml-2 mt-1.5">
               <BurgerButton
                 color={'#eeb30d'}
                 width={32}
